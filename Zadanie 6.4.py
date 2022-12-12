003373cbb2bd0798daef4763d8cbf267ad44170f
mkrzysiek24 <66188338+mkrzysiek24@users.noreply.github.com>
2022-12-13 00:22:35 +0100

    Add files via upload

---
+++ Zadanie 6.4.py
@@ -0,0 +1,13 @@
+def podziel(napis):
+    end = []
+    start = 0
+    for i in range(len(napis) - 1):
+        if napis[i] == " ":
+            start = i + 1
+        elif napis[i] != " " and napis[i + 1] == " ":
+            end.append(napis[start:i + 1])
+    return end
+
+
+s = " Ala ma kota "
+print(podziel(s))
